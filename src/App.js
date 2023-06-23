f184579490daf668f972963b91b870a77a9658b6
Your Name <you@example.com>
2023-06-22 21:24:45 -0700

    Added starter codes in App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="main-board">
+      <p>Today's Date</p>
+      <p>
+        <li>
+        <ul>
+          Wash the dishes
+        </ul>
+        </li>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <li>
+        <ul>
+          Workout
+        </ul>
+      </li>
+    </div>
       </header>
     </div>
   );
